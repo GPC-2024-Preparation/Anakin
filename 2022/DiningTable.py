ec20ce0d535f872e9483d7e193ff8902a06e2650
AshithaGanesan <ashitha.ganesan@hotmail.com>
2024-02-29 12:54:54 +0400

    update

--- 2022/DiningTable.py
+++ 2022/DiningTable.py
@@ -1,11 +1,11 @@
+k = int(input("Enter number of test cases: "))
 
+for i in range(k):
 
+    H = int(input("Enter height of dining table: "))
+    S = int(input("Enter height she can climb: "))
+    N = int(input("Enter number of side tables: "))
 
-
-H = int(input("Enter height of dining table: "))
-S = int(input("Enter height she can climb: "))
-N = int(input("Enter number of side tables: "))
-
-L1 = int(input("Enter length of first table: "))
-L2 = int(input("Enter height of dining table: "))
-L3 = int(input("Enter height of dining table: "))
+    L1 = int(input("Enter length of first table: "))
+    L2 = int(input("Enter length of second table: "))
+    L3 = int(input("Enter length of third table: "))
