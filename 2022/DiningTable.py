40face19d63610a2d29df41cd726bc4fcd9f6c42
AshithaGanesan <ashitha.ganesan@hotmail.com>
2024-02-29 13:00:16 +0400

    updating

--- 2022/DiningTable.py
+++ 2022/DiningTable.py
@@ -1,7 +1,6 @@
 k = int(input("Enter number of test cases: "))
 
 for i in range(k):
-
     H = int(input("Enter height of dining table: "))
     S = int(input("Enter height she can climb: "))
     N = int(input("Enter number of side tables: "))
@@ -10,4 +9,7 @@ L = set()
 for i  in range(N):
     lengthOfTable = int(input("Enter length of table: "))
     L.add(lengthOfTable)
+
+def HeightCheck():
+    pass
 
