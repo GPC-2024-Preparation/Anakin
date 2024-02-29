e31ecb9088cbb495b9f181cffcbaf4d8a63c8edf
AshithaGanesan <ashitha.ganesan@hotmail.com>
2024-02-29 12:52:11 +0400

    prolem A

---
+++ 2022/DiningTable.py
@@ -0,0 +1,11 @@
+
+
+
+
+H = int(input("Enter height of dining table: "))
+S = int(input("Enter height she can climb: "))
+N = int(input("Enter number of side tables: "))
+
+L1 = int(input("Enter length of first table: "))
+L2 = int(input("Enter height of dining table: "))
+L3 = int(input("Enter height of dining table: "))
